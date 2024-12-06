62c42c3051b6ed6784d9f518dc9ba97398d4a5d9
Saradhi0515 <pardhasaradhi.0515@gmail.com>
2024-12-06 21:09:26 +0530

    Updated Main & Tracker

--- PythonPrograms/main.py
+++ PythonPrograms/main.py
@@ -3,19 +3,32 @@ from object_tracker.tracker import Tracker
 
 def main():
 
-    frames = read_video("C:\\Users\\pardh\\Infosys_STMS\\Data\\Video\\Traffic_01.mp4")
-
-    # Video processing and detection
-    detection_of_vehicles = detect_vehicles(frames)
+    frames = read_video("C:\\Users\\pardh\\Infosys_STMS\\Smart_Traffic_Management_System\\Data\\Video\\Traffic_01.mp4")
 
     #object tracking
     obj_tracker = Tracker()
     result = obj_tracker.detect_objects(frames)
-
     output_frames = obj_tracker.draw_annotations(frames, result)
 
 
-    save_video(frames, "C:\\Users\\pardh\\Infosys_STMS\\Output\\Output_01.avi")
+#     save_video(output_frames, "output/123.avi")
+
+###### Main function for 1 image
+
+# def main():
+
+#     frames = read_video("data/image.jpg")
+
+#     #object tracking
+#     obj_tracker = Tracker()
+#     result = obj_tracker.detect_frame(frames)
+#     print(result)
+
+    # output_frames = obj_tracker.draw_annotations(frames, result)
+    #
+    #
+    # save_video(output_frames, "output/output.avi")
 
 if __name__ == '__main__':
     main()
+
