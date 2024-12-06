e5102015458effc84491345dce387320cb884008
Saradhi0515 <pardhasaradhi.0515@gmail.com>
2024-12-06 14:53:12 +0530

    update

--- PythonPrograms/main.py
+++ PythonPrograms/main.py
@@ -3,20 +3,19 @@ from object_tracker.tracker import Tracker
 
 def main():
 
-    frames = read_video("C:\\Users\\pardh\\PycharmProjects\\Infosys_STMS\\Data\\Video\\traffic2.mp4")
+    frames = read_video("C:\\Users\\pardh\\Infosys_STMS\\Data\\Video\\Traffic_01.mp4")
 
     # Video processing and detection
     detection_of_vehicles = detect_vehicles(frames)
 
-    #ojbject Tracker
+    #object tracking
     obj_tracker = Tracker()
     result = obj_tracker.detect_objects(frames)
 
     output_frames = obj_tracker.draw_annotations(frames, result)
 
 
-
-    save_video(frames, "C:\\Users\\pardh\\PycharmProjects\\Infosys_STMS\\Outputs\\AVI\\output2.avi")
+    save_video(frames, "C:\\Users\\pardh\\Infosys_STMS\\Output\\Output_01.avi")
 
 if __name__ == '__main__':
     main()
