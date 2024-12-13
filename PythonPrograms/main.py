477d7adc23e4e7ba20295da4adc721b0e4bc76e4
Saradhi0515 <pardhasaradhi.0515@gmail.com>
2024-12-13 13:38:42 +0530

    Added visualize & new tracker

--- PythonPrograms/main.py
+++ PythonPrograms/main.py
@@ -1,34 +1,30 @@
 from utils.video_utils import read_video, save_video, detect_vehicles
-from object_tracker.tracker_2 import Tracker
-
-def main():
-
-    frames = read_video("C:\\Users\\pardh\\Infosys_STMS\\Smart_Traffic_Management_System\\Data\\Video\\Traffic_01.mp4")
-
-    #object tracking
-    obj_tracker = Tracker()
-    result = obj_tracker.process_video(frames)
-    output_frames = obj_tracker.draw_annotations(frames, result)
-
-
-#     save_video(output_frames, "output/123.avi")
-
-###### Main function for 1 image
+# from object_tracker.tracker_2 import Tracker
 
 # def main():
-
-#     frames = read_video("data/image.jpg")
-
+#
+#     frames = read_video("data/traffic_video.mp4")
+#
 #     #object tracking
 #     obj_tracker = Tracker()
-#     result = obj_tracker.detect_frame(frames)
-#     print(result)
+#     result = obj_tracker.detect_objects(frames)
+#     output_frames = obj_tracker.draw_annotations(frames, result)
+#
+#
+#     output_frames = obj_tracker.process_video(frames)
+#
+#
+#     # save_video(output_frames, "output/123.avi")
 
-    # output_frames = obj_tracker.draw_annotations(frames, result)
-    #
-    #
-    # save_video(output_frames, "output/output.avi")
+
+from object_tracker.tracker_3 import Tracker
+def main():
+    frames = read_video("C:\\Users\\pardh\\Infosys_STMS\\Smart_Traffic_Management_System\\Data\\Video\\Traffic_05.mp4")
+
+    track = Tracker()
+    result = track.process_video(frames)
 
 if __name__ == '__main__':
     main()
+
 
